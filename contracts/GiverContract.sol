389bf51bbd718f790e9593caeef1f9a60fc5e1c0
Paul Mikhaylov <paulmihailov2013@gmail.com>
2021-02-06 13:39:35 +0300

    Update from broxus

---
+++ contracts/GiverContract.sol
@@ -0,0 +1,14 @@
+pragma solidity >= 0.6.0;
+pragma AbiHeader time;
+pragma AbiHeader expire;
+
+contract GiverContract {
+    constructor() {}
+
+    function sendGrams(address dest, uint64 amount)
+        external
+    {
+        tvm.accept();
+        dest.transfer(amount);
+    }
+}
