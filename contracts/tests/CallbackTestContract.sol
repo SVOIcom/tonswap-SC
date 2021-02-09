09ec2449c04745a7891b6236eb57c45aff4a8e18
Pafaul <paulmihailov2013@gmail.com>
2021-02-09 15:41:31 +0300

    update of CallbackTestContract

--- contracts/tests/CallbackTestContract.sol
+++ contracts/tests/CallbackTestContract.sol
@@ -2,38 +2,39 @@ pragma solidity >= 0.6.0;
 pragma AbiHeader time;
 pragma AbiHeader expire;
 
-import "../interfaces/ITransferWalletCallback.sol";
+import "../interfaces/ITokensReceivedCallback.sol";
 
 contract TestContract is ITransferWalletCallback {
     uint256 static _randomNonce;
 
-    address sender_;
-    address receiver_;
+    address sender_address_;
+    address receiver_address_;
     uint128 amount_;
-    uint timestamp_;
-    function internalTransferResult(TvmCell result) external override
-    {
-        tvm.accept();
-        (sender_, receiver_, amount_, timestamp_) = decodeResultCell(result);
-    }
 
-    function decodeResultCell(TvmCell result) 
-        internal 
-        returns (address sender, address receiver, uint128 amount, uint timestamp)
-    {
-        TvmSlice resultSlice = result.toSlice();
-        (sender, receiver, amount, timestamp) = resultSlice.decode(address, address, uint128, uint);
-   }
+    function tokensReceivedCallback(
+        address token_wallet,
+        address token_root,
+        uint128 amount,
+        uint256 sender_public_key,
+        address sender_address,
+        address sender_wallet,
+        address original_gas_to,
+        uint128 updated_balance,
+        TvmCell payload
+    ) external override {
+        sender_address_ = sender_address;
+        receiver_address_ = msg.sender;
+        amount_ = amount;
+    }
 
     function getResult()
         external
         view
-        returns (address sender, address receiver, uint128 amount, uint timestamp)
+        returns (address sender, address receiver, uint128 amount)
     {
         tvm.accept();
-        sender = sender_;
-        receiver = receiver_;
+        sender = sender_address_;
+        receiver = receiver_address_;
         amount = amount_;
-        timestamp = timestamp_;
     }
 }
