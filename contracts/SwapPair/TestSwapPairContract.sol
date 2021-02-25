71d5c2ecc536ab85d31082cb3e5c95a0cefeb00e
DaTaLe <Daniell5771@yahoo.com>
2021-02-25 18:45:07 +0300

    add test swapPair

--- contracts/SwapPair/TestSwapPairContract.sol
+++ contracts/SwapPair/TestSwapPairContract.sol
@@ -20,41 +20,41 @@ contract TestSwapPairContract is ISwapPairContract {
     }
 
 
-    function UserBalanceInfo(UserBalanceInfo ubi) external {
+    function setUserBalanceInfo(UserBalanceInfo ubi) external {
         tvm.accept();
 
         testBalanceInfo = ubi;
     }
 
-    function withdrawToken(address withdrawalTokenRoot, address receiveTokenWallet, uint128 amount) external {
+    function withdrawToken(address withdrawalTokenRoot, address receiveTokenWallet, uint128 amount) override external {
         tvm.accept();
     } 
 
-    function swap(address swappableTokenRoot,  uint128 swappableTokenAmount) external {
+    function swap(address swappableTokenRoot,  uint128 swappableTokenAmount) override external {
         tvm.accept();
     } //-- 
 
-    function getExchangeRate(address swappableTokenRoot, uint128 swappableTokenAmount) external view returns (uint256 rate) {
+    function getExchangeRate(address swappableTokenRoot, uint128 swappableTokenAmount) override external view returns (uint256 rate) {
         tvm.accept();
         return 1;
     } //int twm accept
 
-    function addLiquidity(uint128 firstTokenAmount, uint128 secondTokenAmount) external {
+    function addLiquidity(uint128 firstTokenAmount, uint128 secondTokenAmount) override external {
         tvm.accept();
     } //twm accept
 
-    function getCreationTimestamp() external view returns (uint256 creationTimestamp) {
+    function getCreationTimestamp() override external view returns (uint256 creationTimestamp) {
         tvm.accept();
         return 1;
     } //twm accept int
     
-    function getPairInfo() external returns (SwapPairInfo info) {
+    function getPairInfo() override external returns (SwapPairInfo info) {
         tvm.accept();
         return SwapPairInfo;
 
     } //+ set ret SwapPairInfo
 
-    function getUserBalance() external returns (UserBalanceInfo ubi) {
+    function getUserBalance() override external returns (UserBalanceInfo ubi) {
         tvm.accept();
         return UserBalanceInfo;
 
