ec99ed42591b7225d597ab5eece18451a37e254e
Anton <ntn4work@gmail.com>
2021-03-06 04:11:00 +0300

    swap rates and swap simulation

--- contracts/SwapPair/interfaces/ISwapPairContract.sol
+++ contracts/SwapPair/interfaces/ISwapPairContract.sol
@@ -9,9 +9,9 @@ import './ISwapPairInformation.sol';
 interface ISwapPairContract is ISwapPairInformation {
     function withdrawToken(address withdrawalTokenRoot, address receiveTokenWallet, uint128 amount) external;
 
-    function swap(address swappableTokenRoot,  uint128 swappableTokenAmount) external  returns (uint128 targetTokenAmount);
+    function swap(address swappableTokenRoot,  uint128 swappableTokenAmount) external  returns (SwapInfo _swapInfo);
 
-    function getExchangeRate(address swappableTokenRoot, uint128 swappableTokenAmount) external returns (uint256 rate);
+    function getExchangeRate(address swappableTokenRoot, uint128 swappableTokenAmount) external returns (SwapInfo _swapInfo);
 
     function provideLiquidity(uint128 maxFirstTokenAmount, uint128 maxSecondTokenAmount) external returns (uint128 providedFirstTokenAmount, uint128 providedSecondTokenAmount);
 
@@ -32,16 +32,35 @@ interface ISwapPairContract is ISwapPairInformation {
         uint256 token2LPAmount;
     }
 
+    // struct _DebugERInfo {
+    //     uint256 numerator;
+    //     uint256 denominator;
+    //     uint256 exchangeResultToken1;
+    //     uint256 exchangeResultToken2;
+    // }
+
     struct _DebugERInfo {
-        uint256 numerator;
-        uint256 denominator;
-        uint256 exchangeResultToken1;
-        uint256 exchangeResultToken2;
+        uint256 oldK;
+        uint256 newK;
+
+        uint128 swappableTokenAmount;
+        uint128 targetTokenAmount;
+        uint128 fee;
+
+        uint128 oldFromPool;
+        uint128 oldToPool;
+
+        uint128 newFromPool;
+        uint128 newToPool;
     }
 
     function _getLiquidityPoolTokens() external view returns (_DebugLPInfo dlpi);
 
     function _getUserLiquidityPoolTokens() external view returns (_DebugLPInfo dlpi);
 
-    function _getExchangeRateSimulation(uint256 token1, uint256 token2, uint256 swapToken1, uint256 swapToken2) external view returns (_DebugERInfo deri);
+    // function _getExchangeRateSimulation(uint256 token1, uint256 token2, uint256 swapToken1, uint256 swapToken2) external view returns (_DebugERInfo deri);
+
+    // Тк изначально на контракте нет ликвидности, добавил возможность руками выставить на время тестов (через костыль, но всё же)
+    function _getExchangeRateSimulation(address swappableTokenRoot, uint128 swappableTokenAmount, uint128 fromLP, uint128 toLP) external returns (_DebugERInfo deri);
+
 }
